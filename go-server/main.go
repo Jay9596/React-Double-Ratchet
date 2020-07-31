88d38c6abacdec4834c2e79ca993f3d024a88d39
Jay Shah <Jay9596@users.noreply.github.com>
2020-07-31 13:54:37 -0400

    Update main.go: - Update message struct - Try the decryption - Comments

--- go-server/main.go
+++ go-server/main.go
@@ -11,25 +11,31 @@ import (
 	"github.com/status-im/doubleratchet"
 )
 
+// State handles the state data of the application
 type State struct {
 	sk      []byte
 	user    doubleratchet.Session
 	keyPair doubleratchet.DHPair
 }
 
+// MessageBody is a single message exchanged by the parties.
 type MessageBody struct {
-	Text string `json:"message"`
+	SenderKey string `json:"key"`
+	Text      string `json:"message"`
 }
 
+// KeyExchange is a single time public Key exchange by the parties
 type KeyExchange struct {
 	Key string `json:"key"`
 }
 
+// Ping returns a 200(OK) status and returns with "Ready"
 func ping(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "Ready")
 }
 
+// startComm sends the server's public key and to start the encrypted communication
 func (s *State) startComm(w http.ResponseWriter, r *http.Request) {
 	k := KeyExchange{
 		Key: s.keyPair.PublicKey().String(),
@@ -39,20 +45,42 @@ func (s *State) startComm(w http.ResponseWriter, r *http.Request) {
 	if err := json.NewEncoder(w).Encode(k); err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(k)
+	fmt.Println("Key Exchanged", k)
 
 }
 
 func (s *State) receive(w http.ResponseWriter, r *http.Request) {
 	var m MessageBody
 
+	// Recieve the cipher text and key and read into a MessageBody struct
 	if err := json.NewDecoder(r.Body).Decode(&m); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Write([]byte("Received"))
 
-	// if s.user.RatchetDecrypt([]byte(m.Text), nil) == "hello world" {
+	// fmt.Println("Received Text:", m)
+
+	// TODO: Need to generate proper DH key from received key
+	messHeader := doubleratchet.MessageHeader{
+		DH: []byte(m.SenderKey),
+		N:  1,
+		PN: 0,
+	}
+	mess := doubleratchet.Message{
+		Header:     messHeader,
+		Ciphertext: []byte(m.Text),
+	}
+
+	plaintext, err := s.user.RatchetDecrypt(mess, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(plaintext)) // Test plaintext received
+
+	// if s.user.RatchetDecrypt(mss, nil) == "hello world!" {
 	// 	w.Write([]byte("Experiment Success"))
 	// } else {
 	// 	w.Write([]byte("Need to try Again"))
@@ -61,6 +89,8 @@ func (s *State) receive(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	// Preshared secret key
+	// Copied from the library
 	shared := []byte{
 		0xeb, 0x8, 0x10, 0x7c, 0x33, 0x54, 0x0, 0x20,
 		0xe9, 0x4f, 0x6c, 0x84, 0xe4, 0x39, 0x50, 0x5a,
@@ -68,22 +98,26 @@ func main() {
 		0x1e, 0x2c, 0x9, 0x8d, 0x4b, 0x4d, 0xc1, 0x40,
 	}
 
+	// Generate Key pair for the server
 	keyPair, err := doubleratchet.DefaultCrypto{}.GenerateDH()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Generate a New user with given pre shared key and generated Key pair
 	bob, err := doubleratchet.New([]byte("bob-session-id"), shared, keyPair, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Generate a common state for the server
 	st := &State{
 		sk:      shared,
 		keyPair: keyPair,
 		user:    bob,
 	}
 
+	// Example from Go double ratchet library
 	// // Alice MUST be created with the shared secret and Bob's public key.
 	// alice, err := doubleratchet.NewWithRemoteKey([]byte("alice-session-id"), sk, keyPair.PublicKey(), nil)
 	// if err != nil {
@@ -97,18 +131,16 @@ func main() {
 	// 	log.Fatal(err)
 	// }
 
-	// Which Bob can decrypt.
-	// plaintext, err := bob.RatchetDecrypt(m, nil)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// fmt.Println(string(plaintext))
-
 	router := mux.NewRouter()
 
-	router.HandleFunc("/ping", ping)
-	router.HandleFunc("/start", st.startComm)
-	router.HandleFunc("/recv", st.receive)
-	log.Fatal(http.ListenAndServe(":8080", handlers.CORS()(router)))
+	// Header for CORS handler
+	headersOK := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type"})
+	originsOK := handlers.AllowedOrigins([]string{"*"})
+	methodsOK := handlers.AllowedMethods([]string{"GET", "POST", "OPTIONS", "DELETE", "PUT"})
+
+	router.HandleFunc("/ping", ping).Methods("GET")
+	router.HandleFunc("/start", st.startComm).Methods("GET")
+	router.HandleFunc("/recv", st.receive).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(headersOK, originsOK, methodsOK)(router)))
 }
